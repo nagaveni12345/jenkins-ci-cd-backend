bbb7b5044817a0a08f1709fbbdc07df6249f7976
nagaveni12345 <166248713+nagaveni12345@users.noreply.github.com>
2026-04-15 12:02:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Backend App Deployed Successfully 🚀');
+  res.send('Backend App Deployed Successfully..... 🚀');
 });
 
 app.listen(3000, () => {
